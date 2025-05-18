de7ee50de04ecb28118cc430886e6ba785af3b4f
Giselle Martinez-Sanchez <77756063+GiselleMtnezS@users.noreply.github.com>
2025-05-18 13:26:04 -0500

    CSC450 wk1 CTA

---
+++ School_Snippets/CSC450/wk1/CTA/M1CTA.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+
+//int main() {
+//    std::string firstName = "Alice";
+//    std::string lastName = "Wonderland";
+//    std::string streetAddress = "123 Rabbit Street";
+//    std::string city = "Wonderland";
+//    std::string zipCode = "12345";
+//
+//    std::cout << "First Name: " << firstName << "\n";
+//    std::cout << "Last Name: " << lastName << "\n";
+//    std::cout << "Street Address: " << streetAddress << "\n";
+//    std::cout << "City: " << city << "\n";
+//    std::cout << "ZipCode: " << zipCode << std::endl;
+//
+//    return 0;
+//}
